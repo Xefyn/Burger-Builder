23b48c2d1761194ce5458bd4a9581eeb6cc110c7
richard_ivan <richardivan100499@gmail.com>
2020-08-04 22:37:00 +0700

    add burger builder control

---
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+import BuildControl from "./BuildControl/BuildControl";
+import classes from "./BuildControls.module.css";
+import BurgerBuilder from "../../../container/BurgerBuilder/BurgerBuilder";
+
+const control = [
+  { label: "Salad", type: "salad" },
+  { label: "Bacon", type: "bacon" },
+  { label: "Cheese", type: "cheese" },
+  { label: "Meat", type: "meat" },
+];
+
+const buildControls = (props) => (
+  <div className={classes.BuildControls}>
+    {control.map((ctrl) => (
+      <BuildControl
+        key={ctrl.label}
+        label={ctrl.label}
+        added={() => props.ingredientAdded(ctrl.type)}
+        minus={() => props.ingredientMinus(ctrl.type)}
+        disabled={props.disabled[ctrl.type]}
+      />
+    ))}
+  </div>
+);
+
+export default buildControls;
--- src/container/BurgerBuilder/BurgerBuilder.js
+++ src/container/BurgerBuilder/BurgerBuilder.js
@@ -3,18 +3,47 @@ import React, { useState } from 'react';
 import Aux from '../../hoc/Auxilary'
 
 import Burger from '../../components/Burger/Burger'
+import BuildControls from '../../components/Burger/BuildControls/BuildControls'
+
+const INGREDIENT_PRICES = {
+  salad: 0.5,
+  cheese: 0.4,
+  meat: 1.5,
+  bacon: 0.8,
+}
 
 const BurgerBuilder = () => {
   const [ingredientState,setIngredientState] = useState({
-    meat: 1,
-    salad: 1,
-    cheese: 2,
-    bacon: 2,
+    meat: 0,
+    salad: 0,
+    cheese: 0,
+    bacon: 0,
   });
+  const [priceState,setPriceState] = useState(4);
+  const disabledInfo = {...ingredientState}
+  for(let key in disabledInfo){
+    disabledInfo[key] = disabledInfo[key] <= 0
+  }
+
+  const addIngredientHandler = (type) => {
+    setIngredientState(prevState => ({...prevState,[type]:ingredientState[type] + 1}))
+    setPriceState(prevState => prevState + INGREDIENT_PRICES[type])
+  }
+
+  const minusIngredientHandler = (type) => {
+    if(ingredientState[type] > 0) {
+      setIngredientState(prevState => ({...prevState,[type]:ingredientState[type] - 1}))
+      setPriceState(prevState => prevState - INGREDIENT_PRICES[type])
+    }
+  }
   return(
     <Aux>
       <Burger ingredients={ingredientState}/>
-      <div>Build controls</div>
+      <BuildControls 
+        ingredientAdded={addIngredientHandler}
+        ingredientMinus={minusIngredientHandler}
+        disabled={disabledInfo}
+      />
     </Aux>
   )
 }
